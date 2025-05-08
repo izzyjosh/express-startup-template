5e8ded02cd8ebeef7f6548b2478dc04f6bb6043d
izzyjosh <calebayuba222@gmail.com>
2025-05-08 23:27:39 +0100

    fiix

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,13 @@
 import { app } from "./app";
 import { connectToDatabase } from "./databases/mongoDB";
 import config from "./config/config";
-import serverless from "serverless-http";
 
 const startServer = async () => {
   await connectToDatabase();
-  
-  
-const handler = serverless(app);
 
-// Export the handler for Vercel serverless function
-export default (req, res) => {
-  return handler(req, res);
-};
-  // app.listen(config.port, () => {
-//     console.log(`Server running on port ${config.port}`);
-//   });
+  app.listen(config.port, () => {
+    console.log(`Server running on port ${config.port}`);
+  });
 };
 
 startServer();
